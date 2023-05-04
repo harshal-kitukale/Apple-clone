18481ccc012613ad93b1c4b6c51480c01f6e30fc
Sahil9214 <utkarshsinghal369@gmail.com>
2023-05-04 21:40:20 +0530

    changes video name and Images name and there folder structure

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Signup from "./Authorizations/Signup";
+import Login from "./Authorizations/Login";
 
 function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+  return <div className="App">
+    <Login/>
+  </div>;
 }
 
 export default App;
