45cbabbbcbcb049d4130a9266ee79d2337f75035
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2023-05-04 22:13:26 +0530

    added Homepage,Navbar and Footer

---
+++ Client/src/Abhishek/Footer.jsx
@@ -0,0 +1,168 @@
+import React from 'react'
+import './Styles/footer.css'
+export const Footer = () => {
+  return (
+    <div>
+        <footer style={{zIndex:'30'}} id="footer" data-scroll-section>
+      <div className="footer-container">
+
+      <div className="footer-section-1">
+        <p>Instant savings, otherwise referred to as instant Cashback on the Apple Store Online, is available with the purchase of an eligible product for qualifying HDFC Bank credit cards & HDFC Bank credit card EMI only. Minimum transaction value of ₹10001 applies.Click here to see instant savings amounts and eligible devices. Instant savings are available for up to two orders per rolling 90-day period with an eligible card. Card eligibility is subject to terms and conditions between you and your card-issuing bank. Total transaction value is calculated after any trade-in credit or eligible discount applied. Any subsequent order adjustment(s) or cancellation(s) may result in instant savings being recalculated, and any refund may be adjusted to account for instant savings clawback; this may result in no refund being made to you. Offer may be revised or withdrawn at any time without any prior notice. Terms and conditions apply. Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing. Multiple separate orders cannot be combined for instant savings.</p>
+     
+      <hr/>
+      </div>
+
+      <div className="footer-section-2">
+        <div className="footer-1">
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">Shop and Learn</h4>
+              <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Store</a></li>
+            <li><a href="/">Mac</a></li>
+            <li><a href="/">iPad</a></li>
+            <li><a href="/">iPhone</a></li>
+            <li><a href="/">Watch</a></li>
+            <li><a href="/">AirPods</a></li>
+            <li><a href="/">TV & Home</a></li>
+            <li><a href="/">iPad touch</a></li>
+            <li><a href="/">AirTag</a></li>
+            <li><a href="/">Accessories</a></li>
+          </div>
+          </ul>
+        </div>
+        <div className="footer-2">
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">Services</h4>
+              <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Apple Music</a></li>
+            <li><a href="/">Apple TV+</a></li>
+            <li><a href="/">Apple Arcade</a></li>
+            <li><a href="/">iCloud</a></li>
+            <li><a href="/">Apple One</a></li>
+            <li><a href="/">Apple Books</a></li>
+            <li><a href="/">Apple Podcasts</a></li>
+            <li><a href="/">App Store</a></li>
+          </div>
+          </ul>
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">Account</h4>
+              <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Manage Your Apple ID</a></li>
+            <li><a href="/">Apple Store Account</a></li>
+            <li><a href="/">iCloud.com</a></li>
+            </div>
+          </ul>
+        </div>
+        <div className="footer-3">
+          <ul>
+            <div className="footer-h4-div">
+            <h4 className="footer-h4">Apple Store</h4>
+            <p className="footer-plus">+</p>
+          </div>
+          <div className="panel">
+            <li><a href="/">Ways to Buy</a></li>
+            <li><a href="/">Apple Trade In</a></li>
+            <li><a href="/">Recycling Programme</a></li>
+            <li><a href="/">Order Status</a></li>
+            <li><a href="/">Shopping Help</a></li>
+            </div>
+          </ul>
+        </div>
+        <div className="footer-4">
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">For Business</h4>
+              <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Apple and Business</a></li>
+            </div>
+          </ul>
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">For Education</h4>
+              <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Apple and Education</a></li>
+            <li><a href="/">Shop for Education</a></li>
+            <li><a href="/">Shop for University</a></li>
+            </div>
+          </ul>
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">For Healthcare</h4>
+              <p className="footer-plus">+</p> 
+            </div>
+            <div className="panel">
+            <li><a href="/">Apple in Healthcare</a></li>
+            <li><a href="/">Health on Apple Watch</a></li>
+            </div>
+          </ul>
+        </div>
+        <div className="footer-5">
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">Apple Values</h4>
+               <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Accessibility</a></li>
+            <li><a href="/">Environment</a></li>
+            <li><a href="/">Privacy</a></li>
+            <li><a href="/">Supply Chain</a></li>
+            </div>
+          </ul>
+          <ul>
+            <div className="footer-h4-div">
+              <h4 className="footer-h4">About Apple</h4>
+              <p className="footer-plus">+</p>
+            </div>
+            <div className="panel">
+            <li><a href="/">Newsroom</a></li>
+            <li><a href="/">Apple Leadership</a></li>
+            <li><a href="/">Career Oppurtunities</a></li>
+            <li><a href="/">Investors</a></li>
+            <li><a href="/">Ethics & Compliance</a></li>
+            <li><a href="/">Events</a></li>
+            <li><a href="/">Contact Apple</a></li>
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div className="main-footer">
+        <div className="main-footer-1">
+          <p>More ways to shop: <a className="blue-link" href="/">Find a retailer </a>near you. Or call 000800 040 1966</p>
+        </div>
+      <hr/>
+        <div className="main-footer-2">
+          <div>
+            <p>Copyright © 2023 Apple Inc. All rights reserved.</p>
+          </div>
+          <ul>
+          <li className="first-li-footer"><a href="">Privacy Policy</a></li>
+          <li><a href="">Terms of Use</a></li>
+          <li><a href="">Sales Policy</a></li>
+          <li><a href="">Legal</a></li>
+          <li><a href="">Site Map</a></li>
+        </ul>
+        <div className="footer-country">
+          <a href="/">India</a>
+        </div>
+
+        </div>
+      </div>
+    </div>
+    </footer>
+    </div>
+  )
+}
