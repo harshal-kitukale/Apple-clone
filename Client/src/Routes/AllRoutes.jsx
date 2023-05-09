52147addb816f8dd2e77476c152b2f7524833e1c
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2023-05-09 21:40:08 +0530

    done

--- Client/src/Routes/AllRoutes.jsx
+++ Client/src/Routes/AllRoutes.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Routes ,Route} from 'react-router-dom'
 import { SingleProduct } from '../harshal/SingleProduct'
 import { Home } from '../Abhishek/Home'
-import Products from '../Manju/Products'
 import Login from '../Utkarsh/Authorizations/Login'
 import Signup from '../Utkarsh/Authorizations/Signup'
 import Payment from '../Arun/Cart/Payment'
 import CheckoutPage from '../Arun/Cart/CheckoutPage'
 import Cart from '../harshal/Cart'
 import Generic from '../Utkarsh/UtkarshIpad/Genreric'
+import Store from '../Manju/Store'
 
 const AllRoutes = () => {
   return (
@@ -16,7 +16,7 @@ const AllRoutes = () => {
 
       <Route path={'/singleProduct/:id'} element={<SingleProduct/>}/>
       <Route path={'/'} element={<Home/>}/>
-      <Route path={'/products'} element={<Products/>}/>
+      <Route path={'/products'} element={<Store/>}/>
       <Route path={'/login'} element={<Login/>}/>
       <Route path={'/signup'} element={<Signup/>}/>
       <Route path={'/payment'} element={<Payment/>}/>
